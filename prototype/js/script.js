9cde94e084e51cce81b252d647393c7f34cc5959
Joshua Shirley <joshua_shirley@hotmail.com>
2021-10-17 23:25:46 -0600

    more changes

--- prototype/js/script.js
+++ prototype/js/script.js
@@ -2,14 +2,17 @@ function menuToggle() {
     var nav = document.getElementsByTagName("nav")[0];
     var main = document.getElementsByTagName("main")[0];
     var footer = document.getElementsByTagName("footer")[0];
+    var body = document.getElementBy
     if (nav.style.display === "none") {
         nav.style.display = "block";
         main.style.display = "none";
         footer.style.display = "none";
+        document.body.style.backgroundColor = "#4b465d";
     } else {
         nav.style.display = "none";
         main.style.display = "block";
         footer.style.display = "block";
+        document.body.style.backgroundColor = initial;
     }
 }
 
